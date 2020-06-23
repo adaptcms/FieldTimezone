01eea2996224bd8e1f82cf21cd0f7067250b3908
Charlie Page <charliepage88@gmail.com>
2020-06-23 19:12:57 -0400

    sync

--- src/FieldTimezone.php
+++ src/FieldTimezone.php
@@ -2,7 +2,17 @@
 
 namespace Adaptcms\FieldTimezone;
 
+use Adaptcms\Base\Models\Package;
+
 class FieldTimezone
 {
-    // Build wonderful things
+  /**
+  * On Install
+  *
+  * @return void
+  */
+  public function onInstall()
+  {
+    Package::syncPackageFolder(get_class());
+  }
 }
